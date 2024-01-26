eafc612b814c71691204923875a93e98279bf49c
Little Soft <a0919459151@gmail.com>
2024-01-26 23:34:49 +0800

    Add project files.

---
+++ PasswordHasher.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+
+namespace PasswordHasher
+{
+    public static class PasswordHasher
+    {
+        // Create a password hash
+        public static (string passwordHash, string passwordSalt) CreatePasswordHash(string password)
+        {
+            // Generate a random salt
+            byte[] salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+           
+            // Derive the password hash using PBKDF2 algorithm
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            return (hashed, Convert.ToBase64String(salt));
+        }
+
+        // Verify the password hash
+        public static bool VerifyPasswordHash(string password, string passwordHash, string passwordSalt)
+        {
+            byte[] salt = Convert.FromBase64String(passwordSalt);
+
+            // Derive a hash for the provided password using the same salt
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+
+            // Compare the computed hash with the stored hash
+            return hashed == passwordHash;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using PasswordHasher;
+
+// create passwordhash
+Console.Write("Enter a password: ");
+var password = Console.ReadLine();
+var (passwordHash, passwordSalt) = PasswordHasher.PasswordHasher.CreatePasswordHash(password!);
+Console.WriteLine($"[sys]: passwordSalt: {passwordSalt}");
+Console.WriteLine($"[sys]: passwordHash: {passwordHash}");
+
+Console.WriteLine("");
+Console.WriteLine("===============================================================");
+Console.WriteLine("");
+
+// verify the hash
+Console.Write("Enter a password to verify: ");
+var passwordToVerify = Console.ReadLine();
+var result = PasswordHasher.PasswordHasher.VerifyPasswordHash(passwordToVerify!, passwordHash, passwordSalt);
+
+if(result)
+{
+    Console.WriteLine("[sys]: Password is correct");
+}
+else
+{
+    Console.WriteLine("[sys]: Password is incorrect");
+}
